44cd2644fdfbe1030559b411bb3019a316a8f013
Felipe Micaroni Lalli <micaroni@gmail.com>
2010-07-19 00:18:55 -0300

    Now done!

--- src/br/eti/fml/beautylib/Main.java
+++ src/br/eti/fml/beautylib/Main.java
@@ -3,9 +3,6 @@ package br.eti.fml.beautylib;
 import java.io.File;
 import java.io.IOException;
 
-
-
-
 /**
  *
  * @author Felipe Micaroni Lalli
@@ -13,5 +10,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         ResizeImage ri = new ResizeImage(new File("db/a.jpg"), new File("db/b.jpg"));
+        ri.doResize();
     }
 }
